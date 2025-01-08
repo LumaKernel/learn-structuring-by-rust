9f3c65e72e34dd0517bf731209bf81448da97298
Luma <world@luma.email>
2025-01-08 14:10:23 +0900

    fix square_sum test

--- src/bin/lv0_square_sum.rs
+++ src/bin/lv0_square_sum.rs
@@ -1,11 +1,16 @@
-fn square_sum(v: &[i64]) -> i64 {
+fn square_sum(v: &[i64]) -> u64 {
     todo!()
 }
 
 #[test]
 fn test_square_sum() {
     assert_eq!(square_sum(&vec![1, 2, 3]), 14);
-    assert_eq!(square_sum(&vec![-1, 9, 4, -6]), 122);
+    assert_eq!(square_sum(&vec![-1, 9, 4, -6]), 134);
+    assert_eq!(square_sum(&vec![999, -111, 3342, 5455]), 41936311);
+    assert_eq!(square_sum(&vec![]), 0);
+    assert_eq!(square_sum(&vec![1]), 1);
+    assert_eq!(square_sum(&vec![-1]), 1);
+    assert_eq!(square_sum(&(0..999).collect::<Vec<_>>()), 331835499);
 }
 
 fn main() {
